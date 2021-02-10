983cc631e4c2afa2aac8981e4fad63fbf717221e
Rodion <makarenko.rodion98@gmail.com>
2021-02-10 16:30:29 +0200

    project created

---
+++ Classes/BDContext.php
@@ -0,0 +1,9 @@
+<?php
+
+
+interface BDContext
+{
+ public function getallData();
+ public function saveData();
+ public function DeleteData();
+}
---
+++ Classes/Node.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Node implements  BDContext
+{
+
+    function __construct(){
+
+    }
+    public function getallData(){
+
+    }
+    public function saveData(){
+
+    }
+    public function DeleteData(){
+
+    }
+}
